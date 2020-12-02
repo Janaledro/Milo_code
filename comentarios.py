94bf15af8219072ffac194b1d85684d2a2026eb4
Janaledro <alejandro.garfide@ciencias.unam.mx>
2020-12-02 10:15:24 -0600

    Actualización de comentarios

---
+++ comentarios.py
@@ -0,0 +1,9 @@
+"""
+Los comentarios en python son fundamentales para recordarnos qué hacía nuestro código cuando lo dejamos de usar por
+un tiempo. Nos ayudan a recordar las funciones de éste y sobre todo, a generar anotaciones que creemos importantes. 
+Los comentarios en python y en cualquier lenguaje de programación también nos ayudan a "desactivar" líneas de 
+código que no nos interesan por ahora.
+"""
+
+#Los comentarios se pueden hacer por medio del símbolo numeral (gato) o encerrando el texto en tres comillas dobles 
+#como está hecho en el párrafo de arriba
